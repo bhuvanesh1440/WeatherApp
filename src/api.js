18e344a43f12edc5b41d1f4dfa188adde960404d
bhuvanesh1440 <bhuvanesh1440@gmail.com>
2024-02-28 20:38:31 +0530

    weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "181b3d4ae3mshf93cbc4ea80d912p16c0e2jsna90a739fa7ec",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0c362a24b857c2291122fc2c50633c8b"; // enter your key from openweather API
